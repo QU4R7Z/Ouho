c961b67b72a0b1d3d57d1542a5620512aad5392c
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-08-06 18:32:13 +0900

    g

--- Utils/Components/Windows/BasicWindow.py
+++ Utils/Components/Windows/BasicWindow.py
@@ -1,11 +1,128 @@
 import pygame
 from Utils.Components.Buttons import BasicButton
+from Utils.Graphics import allignment
 
-def draw(screen, screen_x, screen_y, WINDOWS_SIZE_X, WINDOWS_SIZE_Y, WINDOWS_COLOR, WINDOWS_POS_X, WINDOWS_POS_Y, img_x_icon,
+
+def draw(
+    screen,
+    screen_x,
+    screen_y,
+    WINDOWS_SIZE_X,
+    WINDOWS_SIZE_Y,
+    WINDOWS_COLOR,
+    WINDOWS_POS_X,
+    WINDOWS_POS_Y,
+    img_x_icon,
     img_x_icon_x,
-    img_x_icon_y):
+    img_x_icon_y,
+    font,
+    smallfont,
+    footer,
+    footer_text,
+):
     windows_bar = pygame.Surface((WINDOWS_SIZE_X, WINDOWS_SIZE_Y))
     windows_bar.fill(WINDOWS_COLOR)
     screen.blit(windows_bar, (WINDOWS_POS_X, WINDOWS_POS_Y))
+    BasicButton.draw(
+        screen=screen,
+        BUTTON_X=img_x_icon_x,
+        BUTTON_Y=img_x_icon_y,
+        POS_X=allignment.relative_allign(
+            LAYOUT_X=WINDOWS_POS_X,
+            LAYOUT_Y=WINDOWS_POS_Y,
+            LAYOUT_SCALE_X=WINDOWS_SIZE_X,
+            LAYOUT_SCALE_Y=WINDOWS_SIZE_Y,
+            location="X_RIGHT",
+            component_x=img_x_icon_x,
+            component_y=img_x_icon_y,
+            MARGIN_LEVEL=80,
+        ),
+        POS_Y=allignment.relative_allign(
+            LAYOUT_X=WINDOWS_POS_X,
+            LAYOUT_Y=WINDOWS_POS_Y,
+            LAYOUT_SCALE_X=WINDOWS_SIZE_X,
+            LAYOUT_SCALE_Y=WINDOWS_SIZE_Y,
+            location="Y_UP",
+            component_x=img_x_icon_x,
+            component_y=img_x_icon_y,
+            MARGIN_LEVEL=80,
+        ),
+        font=font,
+        text="",
+        text_color=(0, 0, 0),
+        button_color=(0, 0, 0),
+        alpha=None,
+        BTN_NAME="BTN_EXIT_CREATORS_UI",
+        IMAGE=img_x_icon,
+    )
 
+    if footer:
+        footer_bar_y = WINDOWS_SIZE_Y / 100
+        footer_bar = pygame.Surface((WINDOWS_SIZE_X, footer_bar_y))
+        footer_bar.fill((255, 255, 255))
+        screen.blit(
+            footer_bar,
+            (
+                allignment.relative_allign(
+                    LAYOUT_X=WINDOWS_POS_X,
+                    LAYOUT_Y=WINDOWS_POS_Y,
+                    LAYOUT_SCALE_X=WINDOWS_SIZE_X,
+                    LAYOUT_SCALE_Y=WINDOWS_SIZE_Y,
+                    location="X_CENTER",
+                    component_x=WINDOWS_SIZE_X,
+                    component_y=footer_bar_y,
+                ),
+                (
+                    allignment.relative_allign(
+                        LAYOUT_X=allignment.allign(
+                            SCREEN_X=screen_x,
+                            SCREEN_Y=screen_y,
+                            location="X_CENTER",
+                            image_x=WINDOWS_SIZE_X,
+                            image_y=WINDOWS_SIZE_Y,
+                        ),
+                        LAYOUT_Y=allignment.allign(
+                            SCREEN_X=screen_x,
+                            SCREEN_Y=screen_y,
+                            location="Y_CENTER",
+                            image_x=WINDOWS_SIZE_X,
+                            image_y=WINDOWS_SIZE_Y,
+                        ),
+                        LAYOUT_SCALE_X=WINDOWS_SIZE_X,
+                        LAYOUT_SCALE_Y=WINDOWS_SIZE_Y,
+                        location="Y_DOWN",
+                        component_x=WINDOWS_SIZE_X,
+                        component_y=footer_bar_y,
+                        MARGIN_LEVEL=10,
+                    )
+                ),
+            ),
+        )
 
+        footer_text = smallfont.render(footer_text, False, (255, 255, 255))
+        screen.blit(
+            footer_text,
+            (
+                allignment.relative_allign(
+                    LAYOUT_X=WINDOWS_POS_X,
+                    LAYOUT_Y=WINDOWS_POS_Y,
+                    LAYOUT_SCALE_X=WINDOWS_SIZE_X,
+                    LAYOUT_SCALE_Y=WINDOWS_SIZE_Y,
+                    location="X_CENTER",
+                    component_x=footer_text.get_width(),
+                    component_y=footer_text.get_height(),
+                ),
+                (
+                    allignment.relative_allign(
+                        LAYOUT_X=WINDOWS_POS_X,
+                        LAYOUT_Y=WINDOWS_POS_Y,
+                        LAYOUT_SCALE_X=WINDOWS_SIZE_X,
+                        LAYOUT_SCALE_Y=WINDOWS_SIZE_Y,
+                        location="Y_DOWN",
+                        component_x=footer_text.get_width(),
+                        component_y=footer_text.get_height(),
+                        MARGIN_LEVEL=30,
+                    )
+                ),
+            ),
+        )
--- Utils/Graphics/in_game/creators_ui.py
+++ Utils/Graphics/in_game/creators_ui.py
@@ -1,6 +1,7 @@
 import pygame
 from Utils.Graphics import allignment
 from Utils.Components.Buttons import BasicButton
+from Utils.Components.Windows import BasicWindow
 
 
 def draw(
@@ -16,83 +17,34 @@ def draw(
 ):
     BAR_SIZE_X = screen_x / 3
     BAR_SIZE_Y = screen_y / 1.5
-    creator_bar = pygame.Surface((BAR_SIZE_X, BAR_SIZE_Y))
-    creator_bar.fill((0, 0, 0))
-    screen.blit(
-        creator_bar,
-        (
-            allignment.allign(
-                SCREEN_X=screen_x,
-                SCREEN_Y=screen_y,
-                location="X_CENTER",
-                image_x=BAR_SIZE_X,
-                image_y=BAR_SIZE_Y,
-            ),
-            allignment.allign(
-                SCREEN_X=screen_x,
-                SCREEN_Y=screen_y,
-                location="Y_CENTER",
-                image_x=BAR_SIZE_X,
-                image_y=BAR_SIZE_Y,
-            ),
-        ),
-    )
-
-    BasicButton.draw(
+    BasicWindow.draw(
         screen=screen,
-        BUTTON_X=img_x_icon_x,
-        BUTTON_Y=img_x_icon_y,
-        POS_X=allignment.relative_allign(
-            LAYOUT_X=allignment.allign(
-                SCREEN_X=screen_x,
-                SCREEN_Y=screen_y,
-                location="X_CENTER",
-                image_x=BAR_SIZE_X,
-                image_y=BAR_SIZE_Y,
-            ),
-            LAYOUT_Y=allignment.allign(
-                SCREEN_X=screen_x,
-                SCREEN_Y=screen_y,
-                location="Y_CENTER",
-                image_x=BAR_SIZE_X,
-                image_y=BAR_SIZE_Y,
-            ),
-            LAYOUT_SCALE_X=BAR_SIZE_X,
-            LAYOUT_SCALE_Y=BAR_SIZE_Y,
-            location="X_RIGHT",
-            component_x=img_x_icon_x,
-            component_y=img_x_icon_y,
-            MARGIN_LEVEL=80,
+        screen_x=screen_x,
+        screen_y=screen_y,
+        WINDOWS_SIZE_X=BAR_SIZE_X,
+        WINDOWS_SIZE_Y=BAR_SIZE_Y,
+        WINDOWS_COLOR=(0, 0, 0),
+        WINDOWS_POS_X=allignment.allign(
+            SCREEN_X=screen_x,
+            SCREEN_Y=screen_y,
+            location="X_CENTER",
+            image_x=BAR_SIZE_X,
+            image_y=BAR_SIZE_Y,
         ),
-        POS_Y=allignment.relative_allign(
-            LAYOUT_X=allignment.allign(
-                SCREEN_X=screen_x,
-                SCREEN_Y=screen_y,
-                location="X_CENTER",
-                image_x=BAR_SIZE_X,
-                image_y=BAR_SIZE_Y,
-            ),
-            LAYOUT_Y=allignment.allign(
-                SCREEN_X=screen_x,
-                SCREEN_Y=screen_y,
-                location="Y_CENTER",
-                image_x=BAR_SIZE_X,
-                image_y=BAR_SIZE_Y,
-            ),
-            LAYOUT_SCALE_X=BAR_SIZE_X,
-            LAYOUT_SCALE_Y=BAR_SIZE_Y,
-            location="Y_UP",
-            component_x=img_x_icon_x,
-            component_y=img_x_icon_y,
-            MARGIN_LEVEL=80,
+        WINDOWS_POS_Y=allignment.allign(
+            SCREEN_X=screen_x,
+            SCREEN_Y=screen_y,
+            location="Y_CENTER",
+            image_x=BAR_SIZE_X,
+            image_y=BAR_SIZE_Y,
         ),
+        img_x_icon=img_x_icon,
+        img_x_icon_x=img_x_icon_x,
+        img_x_icon_y=img_x_icon_y,
         font=font,
-        text="",
-        text_color=(0, 0, 0),
-        button_color=(0, 0, 0),
-        alpha=None,
-        BTN_NAME="BTN_EXIT_CREATORS_UI",
-        IMAGE=img_x_icon,
+        smallfont=smallfont,
+        footer=True,
+        footer_text="QU4R7Z",
     )
 
     shi3do_text = font.render(language["SHI3DO"], False, (255, 255, 255))
@@ -142,112 +94,6 @@ def draw(
                     component_x=shi3do_text.get_width(),
                     component_y=shi3do_text.get_height(),
                     MARGIN_LEVEL=20,
-                )
-            ),
-        ),
-    )
-
-    footer_bar_y = screen_y / 500
-    footer_bar = pygame.Surface((BAR_SIZE_X, footer_bar_y))
-    footer_bar.fill((255, 255, 255))
-    screen.blit(
-        footer_bar,
-        (
-            allignment.relative_allign(
-                LAYOUT_X=allignment.allign(
-                    SCREEN_X=screen_x,
-                    SCREEN_Y=screen_y,
-                    location="X_CENTER",
-                    image_x=BAR_SIZE_X,
-                    image_y=BAR_SIZE_Y,
-                ),
-                LAYOUT_Y=allignment.allign(
-                    SCREEN_X=screen_x,
-                    SCREEN_Y=screen_y,
-                    location="Y_CENTER",
-                    image_x=BAR_SIZE_X,
-                    image_y=BAR_SIZE_Y,
-                ),
-                LAYOUT_SCALE_X=BAR_SIZE_X,
-                LAYOUT_SCALE_Y=BAR_SIZE_Y,
-                location="X_CENTER",
-                component_x=BAR_SIZE_X,
-                component_y=footer_bar_y,
-            ),
-            (
-                allignment.relative_allign(
-                    LAYOUT_X=allignment.allign(
-                        SCREEN_X=screen_x,
-                        SCREEN_Y=screen_y,
-                        location="X_CENTER",
-                        image_x=BAR_SIZE_X,
-                        image_y=BAR_SIZE_Y,
-                    ),
-                    LAYOUT_Y=allignment.allign(
-                        SCREEN_X=screen_x,
-                        SCREEN_Y=screen_y,
-                        location="Y_CENTER",
-                        image_x=BAR_SIZE_X,
-                        image_y=BAR_SIZE_Y,
-                    ),
-                    LAYOUT_SCALE_X=BAR_SIZE_X,
-                    LAYOUT_SCALE_Y=BAR_SIZE_Y,
-                    location="Y_DOWN",
-                    component_x=BAR_SIZE_X,
-                    component_y=footer_bar_y,
-                    MARGIN_LEVEL=10,
-                )
-            ),
-        ),
-    )
-
-    QU4R7Z_text = smallfont.render("QU4R7Z", False, (255, 255, 255))
-    screen.blit(
-        QU4R7Z_text,
-        (
-            allignment.relative_allign(
-                LAYOUT_X=allignment.allign(
-                    SCREEN_X=screen_x,
-                    SCREEN_Y=screen_y,
-                    location="X_CENTER",
-                    image_x=BAR_SIZE_X,
-                    image_y=BAR_SIZE_Y,
-                ),
-                LAYOUT_Y=allignment.allign(
-                    SCREEN_X=screen_x,
-                    SCREEN_Y=screen_y,
-                    location="Y_CENTER",
-                    image_x=BAR_SIZE_X,
-                    image_y=BAR_SIZE_Y,
-                ),
-                LAYOUT_SCALE_X=BAR_SIZE_X,
-                LAYOUT_SCALE_Y=BAR_SIZE_Y,
-                location="X_CENTER",
-                component_x=QU4R7Z_text.get_width(),
-                component_y=QU4R7Z_text.get_height(),
-            ),
-            (
-                allignment.relative_allign(
-                    LAYOUT_X=allignment.allign(
-                        SCREEN_X=screen_x,
-                        SCREEN_Y=screen_y,
-                        location="X_CENTER",
-                        image_x=BAR_SIZE_X,
-                        image_y=BAR_SIZE_Y,
-                    ),
-                    LAYOUT_Y=allignment.allign(
-                        SCREEN_X=screen_x,
-                        SCREEN_Y=screen_y,
-                        location="Y_CENTER",
-                        image_x=BAR_SIZE_X,
-                        image_y=BAR_SIZE_Y,
-                    ),
-                    LAYOUT_SCALE_X=BAR_SIZE_X,
-                    LAYOUT_SCALE_Y=BAR_SIZE_Y,
-                    location="Y_DOWN",
-                    component_x=QU4R7Z_text.get_width(),
-                    component_y=QU4R7Z_text.get_height(),
-                    MARGIN_LEVEL=30,
                 )
             ),
         ),
